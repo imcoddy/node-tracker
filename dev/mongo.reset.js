e7a6b7c1f64fd8cdbe5cbc4a2fb118ed7acbdc46
Qiu Di <imcoddy@gmail.com>
2011-06-29 10:14:23 +0800

    add reseting indexes as well

--- dev/mongo.reset.js
+++ dev/mongo.reset.js
@@ -1,2 +1,6 @@
 db.apps.remove();
 db.records.remove();
+db.apps.dropIndexes();
+db.records.dropIndexes();
+db.apps.ensureIndex({app_name:1,platform:1});
+db.records.ensureIndex({app_id:1,tag:1,time:1});
